3ffea6a3dccad493ff3b6021de46e3673285b7c5
Alexander Martynov <mas@van.work>
2020-01-13 11:27:21 +0300

    fixed typo

--- explorer/explorer/routes.py
+++ explorer/explorer/routes.py
@@ -1,7 +1,7 @@
 from .views import signin, signup, wallet_create, wallet_list
 
 def setup_routes(app):
-    app.router.add_post('/singup', signup)
-    app.router.add_post('/singin', signin)
+    app.router.add_post('/signup', signup)
+    app.router.add_post('/signin', signin)
     app.router.add_get('/wallets', wallet_list)
     app.router.add_post('/wallets', wallet_create)
