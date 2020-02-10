f673b733a2cc2b37b1e8d1748601517058ad1c9f
Alexander Martynov <mas@van.work>
2020-02-10 06:02:01 +0300

    локализировано описание контракта

--- explorer/main.py
+++ explorer/main.py
@@ -1,7 +1,6 @@
 from logging import getLogger, basicConfig
 
 from aiohttp import web
-
 from explorer.application import make_app
 
 
@@ -11,4 +10,4 @@ logger = getLogger(__name__)
 app = make_app()
 
 if __name__ == '__main__':
-    web.run_app(app)
+    web.run_app(app, host='0.0.0.0', port=8080)
